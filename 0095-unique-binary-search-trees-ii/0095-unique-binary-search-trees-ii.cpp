2715b2b0520d18eb69123155fd932881fc09527b
Harshil Savaliya <132802415+harshil3030@users.noreply.github.com>
2024-06-06 21:27:27 +0530

    Time: 11 ms (55.04%), Space: 16.3 MB (88.14%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -48,5 +48,32 @@ public:
     vector<TreeNode*> generateTrees(int n) {
         map<pair<int, int>, vector<TreeNode*>> memo;
         return gen(1, n, memo);
+        
+        /*
+         this problem is about the bst and not bt
+         so all the left side will be small 
+         and all the right side will be big 
+         
+         we will generate from both side 
+         take those left roots, and right roots
+         
+         match all the right roots with the each and every left roots or vice versa 
+         set the root -> left 
+         and root -> right 
+         
+         push_back root in result
+         
+         Note : always create new root don't use the same root to get the different combination 
+         otherwise it will create the duplicates 
+         
+         why ?
+         assume you are returning the result at that time all the result array will be created with the same root 
+         so it will provide the same tree in whole array!
+         
+         do mapping with start, end -> vector<TreeNode*>
+         
+         {start, end} -> vector<TreeNode*>
+        
+        */
     }
 };

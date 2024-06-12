b4492ee484d8d5f00f5f5b81844b66dde1cdc54d
Harshil Savaliya <132802415+raiden3030@users.noreply.github.com>
2024-06-12 23:58:35 +0530

    Time: 11 ms (87.33%), Space: 19 MB (45.56%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int cnt = 0;
     
-    void fun(TreeNode* root, int target, vector<long long> tmp) {
+    void fun(TreeNode* root, int target, vector<long long> &tmp) {
         
         if (!root) return;
         
@@ -28,28 +28,29 @@ public:
         tmp.push_back(root -> val);
         fun(root -> left, target, tmp);
         fun(root -> right, target, tmp);
+        tmp.pop_back();
     }
      
-    void secondway(TreeNode* root, int target, long long curr, map<long long, int> &mp) {
-        if (!root) return;
-        curr += root -> val;
+//     void secondway(TreeNode* root, int target, long long curr, map<long long, int> &mp) {
+//         if (!root) return;
+//         curr += root -> val;
         
-        cnt += mp[{curr - target}];
-        mp[curr]++;
+//         cnt += mp[{curr - target}];
+//         mp[curr]++;
         
-        secondway(root -> left, target, curr, mp);
-        secondway(root -> right, target, curr, mp);
+//         secondway(root -> left, target, curr, mp);
+//         secondway(root -> right, target, curr, mp);
         
-        mp[curr]--;
+//         mp[curr]--;
         
-    }
+//     }
     
     int pathSum(TreeNode* root, int targetSum) {
-        // vector<long long> tmp;
-        // fun(root, targetSum, tmp);
-        map<long long ,int> mp;
-        mp[0] = 1;
-        secondway(root, targetSum,0,mp);
+        vector<long long> tmp;
+        fun(root, targetSum, tmp);
+        // map<long long ,int> mp;
+        // mp[0] = 1;
+        // secondway(root, targetSum,0,mp);
         
         return cnt;
     }

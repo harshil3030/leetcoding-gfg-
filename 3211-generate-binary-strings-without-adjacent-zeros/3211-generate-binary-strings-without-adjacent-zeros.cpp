9e3c8b7c234cb32f119a8833e9920d4c19c1a175
Harshil Savaliya <132802415+itsme3030@users.noreply.github.com>
2024-07-07 17:42:52 +0530

    Time: 30 ms (40.00%), Space: 32.4 MB (20.00%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     void fun(vector<string> &ans, int &n, int level, string tmp) {
         if (level == n) {
-            
-            bool ok = true;
-            for(int i = 0; i < tmp.size() - 1; i++) {
-                if (tmp[i] == tmp[i+1] && tmp[i] == '0') {
-                    ok = false;
-                }
-            }
-            
-            if(ok) ans.push_back(tmp);
+         
+          ans.push_back(tmp);
             return;
         }
         tmp += '0';
-        fun(ans, n, level + 1, tmp);
+        if ((tmp.size() > 1 && tmp[tmp.size()-2] != '0') || tmp.size() == 1) {
+            fun(ans, n, level + 1, tmp);
+        }
+        
         tmp.pop_back();
         tmp += '1';
         fun(ans, n, level + 1, tmp);

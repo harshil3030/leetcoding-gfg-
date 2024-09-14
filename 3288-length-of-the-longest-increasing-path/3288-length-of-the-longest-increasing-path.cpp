e8e0eb4354bdbce20168589f8c1d7d52347f0220
Harshil Savaliya <132802415+itsme3030@users.noreply.github.com>
2024-09-14 23:31:44 +0530

    Time: 400 ms (100.00%), Space: 149.5 MB (81.82%) - LeetHub

--- 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
+++ 3288-length-of-the-longest-increasing-path/3288-length-of-the-longest-increasing-path.cpp
@@ -4,7 +4,7 @@ public:
         int n = c.size();
         int m = c[0].size();
         
-        auto comp = [&](vector<int> &v1, vector<int> &v2) {
+        auto comp = [](vector<int> &v1, vector<int> &v2) {
             if (v1[0] == v2[0]) return v1[1] > v2[1];
             return v1[0] < v2[0];
         };
